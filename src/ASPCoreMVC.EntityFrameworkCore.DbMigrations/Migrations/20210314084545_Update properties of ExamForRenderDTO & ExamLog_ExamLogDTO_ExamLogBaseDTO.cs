c6965844f82870c5a25403ea33e198620ec24cb2
Vistark <services.vistark@gmail.com>
2021-03-14 15:48:38 +0700

    .

---
+++ src/ASPCoreMVC.EntityFrameworkCore.DbMigrations/Migrations/20210314084545_Update properties of ExamForRenderDTO & ExamLog_ExamLogDTO_ExamLogBaseDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ASPCoreMVC.Migrations
+{
+    public partial class UpdatepropertiesofExamForRenderDTOExamLog_ExamLogDTO_ExamLogBaseDTO : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "ExamCategoryId",
+                table: "ASPCoreMVC_ExamLog",
+                type: "char(36)",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExamCategoryId",
+                table: "ASPCoreMVC_ExamLog");
+        }
+    }
+}
