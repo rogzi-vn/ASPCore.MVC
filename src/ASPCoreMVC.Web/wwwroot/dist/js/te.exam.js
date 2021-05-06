67f648fb642d8c4a7295ee34c3dfcd202ffb9c85
Vistark <services.vistark@gmail.com>
2021-05-06 14:53:34 +0700

    Xong phần căn chỉnh hiển thị cho mathJax

--- src/ASPCoreMVC.Web/wwwroot/dist/js/te.exam.js
+++ src/ASPCoreMVC.Web/wwwroot/dist/js/te.exam.js
@@ -313,3 +313,62 @@ function collectAnswers() {
     });
     return answers;
 }
+
+// Xử lý bổ xung khi câu hỏi của mathJax bị tràn
+window.addEventListener('resize', function () {
+    optimizeMathJaxAnswer();
+});
+
+function optimizeMathJaxAnswer() {
+    // Lấy danh sách các khối câu hỏi
+    $(".question-container").each(function () {
+        const ansList = $(this).children(".row").children(".col-12");
+        console.log(`Số câu hỏi đếm được: ${ansList.length}`);
+        if (ansList && ansList.length > 0) {
+            // Đếm số cột của câu trả lời thuộc câu hỏi này
+            let columnCount = 0;
+            const matched = $(ansList[0]).attr("class").match(/col-lg-(\d+)/m)[1];
+            if (matched && matched > 0) {
+                columnCount = parseInt(matched);
+            }
+            console.log(`Số cột: ${columnCount}`);
+
+            // Kích thước của mỗi ô
+            let maxColWidth = $(ansList[0]).width();
+            console.log(`Kích thước thực mỗi ô: ${maxColWidth}`);
+
+            let isHaveMathJax = false;
+
+            // Kích thước lớn nhất của câu trả lời
+            let maxAnsWidth = 0;
+            ansList.each(function () {
+                var p = $(this);
+                var mathFrame = p.children(".MathJax");
+                console.log(`Số lượng MathJax đếm được: ${mathFrame.length}`);
+                if (mathFrame && mathFrame.length > 0) {
+                    isHaveMathJax = true;
+                    const crrWidth = $(mathFrame[0]).width();
+                    if (crrWidth > maxAnsWidth) {
+                        maxAnsWidth = crrWidth;
+                    }
+                }
+            });
+
+            console.log(`Kích thước câu trả lời lớn nhất: ${maxAnsWidth}`);
+
+            if (isHaveMathJax) {
+                const overSizeRate = maxAnsWidth / maxColWidth;
+                console.log(`Tỉ lệ thay đổi kích thước: ${overSizeRate}`);
+                if (overSizeRate > 1 && overSizeRate <= 2 && columnCount == 3) {
+                    ansList.removeClass(`col-lg-${columnCount}`)
+                        .addClass(`col-lg-${columnCount * 2}`);
+                } else if (overSizeRate > 2 && columnCount < 12) {
+                    ansList.removeClass(`col-lg-${columnCount}`)
+                        .addClass(`col-lg-12`);
+                }
+            }
+        }
+    });
+}
+
+optimizeMathJaxAnswer();
